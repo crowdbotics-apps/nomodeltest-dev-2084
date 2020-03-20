e4ca13a36b8fc43e13a1764ffe8113d233b364d9
Crowdbotics <team@crowdbotics.com>
2020-03-20 15:45:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nomodeltest_dev_2084/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nomodeltest_dev_2084;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "nomodeltest_dev_2084";
+    }
+}
